53ecf21f5a5d194c5f899743c0d5e8636e5bce4f
Ken_Jones <ken.jones.z.music@gmail.com>
2024-02-05 03:38:57 +0000

    settings page, interpage navigation between it and home page, using icon buttons in appBar

--- lib/concrete_layer/home_page.dart
+++ lib/concrete_layer/home_page.dart
@@ -1,12 +1,30 @@
-// lib/concrete_layer/home_page.dart
 import 'package:flutter/material.dart';
 import '../abstract_layer/abstract_base_page.dart';
+import 'settings_page.dart'; // Import the settings page
 
 class HomePage extends AbstractBasePage {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Home Page Content Goes Here'),
+      ),
+    );
   }
 }
